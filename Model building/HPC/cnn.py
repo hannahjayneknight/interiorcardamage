8d7f440ab36d94e552f67163d6b05de9f0cf511e
Hannah Jayne Knight <hannah@tinpot.co.uk>
2023-03-24 12:34:02 +0000

    Trying to improve CNN

---
+++ Model building/HPC/cnn.py
@@ -0,0 +1,42 @@
+#Load libraries
+import os
+import numpy as np
+import torch
+import glob
+import torch.nn as nn
+from torchvision.transforms import transforms
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+from torch.autograd import Variable
+import torchvision
+import pathlib
+
+device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+'''
+
+transformer=transforms.Compose([
+    transforms.Resize((150,150)),
+    transforms.RandomHorizontalFlip(), # flips image with p=0.5 to augment data
+    transforms.ToTensor(),  #0-255 to 0-1, numpy to tensors
+    transforms.Normalize([0.5,0.5,0.5], # 0-1 to [-1,1] , formula (x-mean)/std
+                        [0.5,0.5,0.5])
+])
+
+train_path = '../Data/train'
+test_path = '../Data/test'
+
+train_loader=torch.utils.data.DataLoader(
+    torchvision.datasets.ImageFolder(root=train_path, transform=transformer),
+    batch_size=64, shuffle=True
+)
+test_loader=torch.utils.data.DataLoader(
+    torchvision.datasets.ImageFolder(root=test_path, transform=transformer),
+    batch_size=32, shuffle=True
+)
+
+root=pathlib.Path(train_path)
+classes=sorted([j.name.split('/')[-1] for j in root.iterdir()])
+print(len(classes))
+
+'''
