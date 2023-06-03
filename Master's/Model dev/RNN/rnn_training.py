0e30842f038a77e5c2b74c68d516ad07c8041422
Hannah Jayne Knight <hannah@tinpot.co.uk>
2023-06-03 12:10:13 +0100

    adding results, final models and final rnn code

--- Master's/Model dev/RNN/rnn_training.py
+++ Master's/Model dev/RNN/rnn_training.py
@@ -4,6 +4,8 @@ import torch.optim as optim
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
+from torch.utils.data import Subset
+import numpy as np
 from rnn import RNN
 
 # Set device
@@ -15,25 +17,30 @@ input_size = image_size * image_size * 3  # Image size: 32x32 with 3 color chann
 hidden_size = 128
 num_layers = 2
 num_classes = 2
-batch_size = 100
-num_epochs = 10
+num_epochs = 100
 learning_rate = 0.001
 
 # Data transforms
 transformer = transforms.Compose([
-    transforms.Resize((image_size, image_size)),     # Resize the image to 32x32
-    transforms.ToTensor(),           # Convert image to tensor
-    transforms.Normalize(            # Normalize image channels
+    transforms.Resize((image_size, image_size)),
+    transforms.ToTensor(),
+    transforms.Normalize(
         mean=[0.485, 0.456, 0.406],
         std=[0.229, 0.224, 0.225]
     )
 ])
 
 # Load the dataset
-train_path = "../foreign_vs_clear/train"
+train_path = "../Data/train"
+train_dataset = ImageFolder(root=train_path, transform=transformer)
+
+n = 1275
+
+train_dataset_subset = Subset(train_dataset, np.random.choice(len(train_dataset), n, replace=False))
+
 train_loader=DataLoader(
-    ImageFolder(root=train_path, transform=transformer),
-    batch_size=batch_size, shuffle=True, num_workers=4,
+    train_dataset_subset,
+    batch_size=32, shuffle=True, num_workers=4,
     pin_memory=True,
 )
 
